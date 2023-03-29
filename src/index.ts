491809704347567385e6e87be48ac4ee38bee7f0
Sungin Lee <dlrkehrud@naver.com>
2023-03-29 13:19:53 +0900

    5.4 JSDoc, allowJs, .d.ts대신 /** - .d.ts 대신 /** */ 형식으로 ts보호를 받을 수 있음. js에서 바로 적용이 가능하다!!!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 // .t.ts 에서 타입을 지정해 줘야 사용 가능
-import { init, exit } from "myPackage";
+import { init, exit } from "./myPackage";
 
-init({
-  url: "true",
-});
-
-exit(1);
+init();
--- src/myPackage.d.ts
+++
@@ -1,8 +0,0 @@
-interface Config {
-  url: string;
-}
-// type을 지정해 주기 위해 사용
-declare module "myPackage" {
-  function init(config: Config): boolean;
-  function exit(code: number): number;
-}
